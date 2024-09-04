65c33f9fcb138fc52ed3284a9519bab8cba63287
kitovskiy <kitovskiy1@gmail.com>
2024-09-04 20:06:56 +0400

    update gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -8,6 +8,9 @@ const program = new Command();
 program
   .version('0.0.1')
   .description('Compares two configuration and shows a difference')
+  .arguments('filepath1')
+  .arguments('filepath2')
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
 
 program.parse();
