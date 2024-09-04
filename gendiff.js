41b4b959626da694544cb0ed0380768920ff22a7
kitovskiy <kitovskiy1@gmail.com>
2024-09-04 19:27:50 +0400

    update package.json, add node_modules

---
+++ gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+//@ts-check
+
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration and shows a difference')
+  .helpOption('-h, --help', 'output usage information')
+
+program.parse(process.argv);
