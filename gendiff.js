00f62b9c21d89d8948b42454e358a6da50b997ad
kitovskiy <kitovskiy1@gmail.com>
2024-09-05 21:17:52 +0400

    added fucntion for parsing

--- gendiff.js
+++ gendiff.js
@@ -3,7 +3,16 @@
 //@ts-check
 
 import { Command } from 'commander';
+import * as fs from 'fs';
+import path from 'path';
+
 const program = new Command();
+
+const parseFile = (filepath) => {
+  const absoluteParse = path.resolve(process.cwd(), filepath); // Приводим к абсолютным путям
+  const fileContent = fs.readFileSync(absoluteParse, 'utf-8'); // Сравниваем синхронно
+  return JSON.parse(fileContent);
+}
 
 program
   .version('0.0.1')
@@ -12,5 +21,12 @@ program
   .arguments('filepath2')
   .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2) => {
+    const data1 = parseFile(filepath1);
+    const data2 = parseFile(filepath2);
 
-program.parse();
+    console.log('Parsed data from file1:', data1);
+    console.log('Parsed data from file2:', data2);
+  })
+  
+program.parse(process.argv);
