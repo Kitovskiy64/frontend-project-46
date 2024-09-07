ebf92dae9e850f80af4f62ba0a9a79123e641845
kitovskiy <kitovskiy1@gmail.com>
2024-09-07 10:26:38 +0400

    add function to diff to paths

--- gendiff.js
+++ gendiff.js
@@ -5,13 +5,35 @@
 import { Command } from 'commander';
 import * as fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
 const program = new Command();
 
 const parseFile = (filepath) => {
-  const absoluteParse = path.resolve(process.cwd(), filepath); // Приводим к абсолютным путям
-  const fileContent = fs.readFileSync(absoluteParse, 'utf-8'); // Сравниваем синхронно
+  const absoluteParse = path.resolve(process.cwd(), filepath);
+  const fileContent = fs.readFileSync(absoluteParse, 'utf-8');
   return JSON.parse(fileContent);
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const data1 = parseFile(filepath1);
+  const data2 = parseFile(filepath2);
+
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+
+  const result = keys.map((key) => {
+    if(!_.has(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if(!_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if(data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    }
+    return `    ${key}: ${data1[key]}`;
+  })
+  return `{\n${result.join('\n')}\n}`;
 }
 
 program
@@ -22,11 +44,8 @@ program
   .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
   .action((filepath1, filepath2) => {
-    const data1 = parseFile(filepath1);
-    const data2 = parseFile(filepath2);
-
-    console.log('Parsed data from file1:', data1);
-    console.log('Parsed data from file2:', data2);
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
   })
   
 program.parse(process.argv);
