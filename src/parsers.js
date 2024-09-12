214c0f3593d5e5974ca5afd47ec0613704c29057
kitovskiy <kitovskiy1@gmail.com>
2024-09-12 20:52:48 +0400

    parser logic in separate module

--- src/parsers.js
+++ src/parsers.js
@@ -2,3 +2,21 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
+const parseFile = (filepath) => {
+  const absoluteParse = path.resolve(process.cwd(), filepath);
+  const fileContent = fs.readFileSync(absoluteParse, 'utf-8');
+
+  const ext = path.extname(filepath).toLowerCase();
+
+  switch (ext) {
+    case '.json':
+      return JSON.parse(fileContent);
+    case '.yml':
+    case '.yaml':
+      return yaml.load(fileContent);
+    default:
+      throw new Error(`Unsupported file format: ${ext}`);
+  }
+};
+
+export default parseFile;
