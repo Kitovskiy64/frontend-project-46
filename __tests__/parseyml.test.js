7ce44a203ba6d7d4bedff53bdb05cba7b58c610f
kitovskiy <kitovskiy1@gmail.com>
2024-09-12 19:59:25 +0400

    add tests for 2 yml files

---
+++ __tests__/parseyml.test.js
@@ -0,0 +1,17 @@
+import path from 'path';
+import genDiff from '../src/gendiff';
+
+test('gendiff для сравнения двух плоских yml файлов', () => {
+  const filepath1 = path.resolve('__fixtures__', 'file1.yml');
+  const filepath2 = path.resolve('__fixtures__', 'file2.yml');
+  const expected = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+  }`;
+
+  expect(genDiff(filepath1, filepath2).toBe(expected));
+})
