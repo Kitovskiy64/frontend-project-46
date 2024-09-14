da3f22b7ec0b2d6d875af89d2ce4a72f625d218c
kitovskiy <kitovskiy1@gmail.com>
2024-09-14 11:19:37 +0400

    fix cognetive complexity in function

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,25 +5,27 @@
 import _ from 'lodash';
 import parseFile from './parsers.js';
 
+const formatDiff = (key, data1, data2) => {
+  if(!_.has(data2, key)) {
+    return `  - ${key}: ${data1[key]}`;
+  }
+  if(!_.has(data1, key)) {
+    return `  + ${key}: ${data2[key]}`;
+  }
+  if(data1[key] !== data2[key]) {
+    return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+  }
+  return `    ${key}: ${data1[key]}`;
+};
+
 const genDiff = (filepath1, filepath2) => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
-
+  
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
-
-  const result = keys.map((key) => {
-    if(!_.has(data2, key)) {
-      return `  - ${key}: ${data1[key]}`;
-    }
-    if(!_.has(data1, key)) {
-      return `  + ${key}: ${data2[key]}`;
-    }
-    if(data1[key] !== data2[key]) {
-      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
-    }
-    return `    ${key}: ${data1[key]}`;
-  })
+  const result = keys.map((key) => formatDiff(key, data1, data2));
+  
   return `{\n${result.join('\n')}\n}`;
-}
+};
 
 export default genDiff;
