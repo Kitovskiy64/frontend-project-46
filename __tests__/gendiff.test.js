a55b84b3a2ca0769239306716fc38a1b5cbbd266
kitovskiy <kitovskiy1@gmail.com>
2024-09-14 11:21:15 +0400

    fix similar blocks in code

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,13 @@
 import path from 'path';
 import genDiff from "../src/gendiff.js";
 
+const runTest = (ext, expected) => {
+  const filepath1 = path.resolve('__fixtures__', `file1.${ext}`);
+  const filepath2 = path.resolve('__fixtures__', `file2.${ext}`)
+  expect(genDiff(filepath1, filepath2).toBe(expected));
+}
+
 test('gendiff сравнение двух плоских JSON файлов', () => {
-  const filepath1 = path.resolve('__fixtures__', 'file1.json');
-  const filepath2 = path.resolve('__fixtures__', 'file2.json')
   const expected = `{
   - follow: false
     host: hexlet.io
@@ -13,6 +17,19 @@ test('gendiff сравнение двух плоских JSON файлов', () 
   + verbose: true
 }`;
 
-  expect(genDiff(filepath1, filepath2)).toBe(expected);
+  runTest('json', expected);
 });
+
+test('gendiff сравнение двух плоских yml файлов', () => {
+  const expected = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+  runTest('yml', expected);
+})
 
